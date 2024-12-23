d47f146511af457497fc8f497a6272679db91c8f
Brian Turchyn <brian@dj-bri-t.net>
2024-12-23 10:22:01 -0600

    Updates

--- classes/telegram.js
+++ classes/telegram.js
@@ -2,5 +2,9 @@
 exports.__esModule = true;
 var Telegraf = require("telegraf");
 var bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+bot = bot.catch((e, ctx) => {
+  console.error("Telegram error", e, ctx);
+});
+
 module.exports.bot = bot;
 //# sourceMappingURL=telegram.js.map
--- classes/telegram.ts
+++ classes/telegram.ts
@@ -3,7 +3,9 @@ declare var require: any;
 // Telegram setup
 const Telegraf = require("telegraf");
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN).catch((e, ctx) => {
+  console.error("Telegram error", e, ctx)
+});
 
 module.exports.bot = bot;
 export {};
